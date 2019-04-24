bdc489c6d5751d0fa86370ef12f0cf8e6bf3a401
Magnus Johnson <magnus@magmhj.com>
2019-04-24 02:15:27 -0400

    Added more addons support

--- addons.py
+++ addons.py
@@ -1,9 +1,25 @@
 
 
 
-class AddonsModifier(object):
+class AddonsModifier(object): # loads saved modifications and applies them to current page
     def __init__(self, page):
         self.page = page
 
     def generate(self):
         return self.page
+
+class AddonsServer(object): # saves modifications to file
+    def __init__(self, create_page_filename):
+        self.create_page_filename = create_page_filename
+        self.addons = []
+
+    def on_update(self):
+        for addon in self.addons:
+            addon.get_modifications()
+
+class Addon(object):
+    def __init__(self, page):
+        self.page = page
+
+    def get_modifications(self):
+        return None
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import os
 import time
 import webbrowser
 import threading
+from addons import *
 
 import http.server
 import socketserver
@@ -23,7 +24,9 @@ dev_page_middle = '\\'+'r'+'\\'+'n'+'|'+'\\'+'n'+'|'+'\\'+'r'
 dev_page_suffix = ''')|$)/gm)
                 for (var i = 0; i < lines.length; i++) {
                     if (lines[i] === 'refresh') {
-                        location.reload(false);
+                        // location.reload(false);
+                        var iframe = document.getElementsByName('content_window')[0];
+                        iframe.contentWindow.location.reload(true);
                     }
                 }
             } else {
@@ -41,12 +44,13 @@ dev_page_suffix = ''')|$)/gm)
 </script>
 
 <body style='padding: 0px; margin: 0px;'>
-<iframe src='./site/index.html' style='width: 100%; height: 100%; outline: none;' frameborder='0'></iframe>
+<iframe src='./site/index.html' name='content_window' style='width: 100%; height: 100%; outline: none;' frameborder='0'></iframe>
 </body>
 
 </html>
 '''
 dev_page = dev_page_prefix+dev_page_middle+dev_page_suffix
+addons_server = AddonsServer("create_page.py")
 
 def main_loop():
     while True:
@@ -74,6 +78,10 @@ def main_loop():
 
         time.sleep(0.25)
 
+def addons_loop():
+    global addons_server
+    addons_server.on_update()
+
 # creates dev_server.html
 dev_server_page = open("dev_server.html","w") 
 dev_server_page.write(dev_page)
@@ -83,14 +91,16 @@ dev_server_page.close()
 port = 8080
 handler = http.server.SimpleHTTPRequestHandler
 t1 = threading.Thread(target=main_loop)
+t2 = threading.Thread(target=addons_loop)
 with socketserver.TCPServer(("", port), handler) as httpd:
 
     # opens web browser of local server
     webbrowser.open('http://127.0.0.1:8080/dev_server.html', new=0, autoraise=True)
     print("SwiftPage server running, your site will now be automatically regenerated when changes are made")
  
-    # starts main loop
+    # starts loops
     t1.start()
+    t2.start()
 
     # serves html server
     httpd.serve_forever()
